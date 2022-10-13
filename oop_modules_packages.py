a3885c51657998dd5fe9a1f88a197e46d013c7ea
Abhishek Jha <abhishek.jha.uk@gmail.com>
2022-10-13 12:32:36 +0100

    uddate

--- oop_modules_packages.py
+++ oop_modules_packages.py
@@ -1,5 +1,18 @@
 # Example Use Case - Lucky Draw
+# import random
 
-import random
+# from random import random
 
-print(random.random())
+# print(random.random())
+# print(random())
+
+# Each time it's run generates a random number - 0-1
+# calculate DOB or year of birth
+
+import math
+
+number = 23.66
+# number 1.50 above to round up 2
+# number 1.49 less round down to  1
+print(math.ceil(number))
+print(math.floor(number))
