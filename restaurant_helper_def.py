ca16fae4fa9046f5b1df91c23285995746400173
Abhishek Jha <abhishek.jha.uk@gmail.com>
2022-10-13 20:46:53 +0100

    restaurant

--- restaurant_helper_def.py
+++ restaurant_helper_def.py
@@ -1,4 +1,4 @@
-# Define a method that contains the menu item
+# Define a method that contains the menu item and returns
 def menu_items(category):
     menu_list = {
         "starter": ["Salmon Platter", "Oyesters", "Pappadum", "Empanadilas", "Vol-au-vents", "Dumpling", "Canape"],
@@ -16,22 +16,24 @@ def menu_items(category):
 
 
 # Define a method that displays the items
-def display_items(items):
+def display_items(items, category):
+    print(f"\n ***** {category.capitalize()} list *****")  # For formatting purpose only
     for item in range(len(items)):
-        print(item+1, ".", items[item])
+        print(item+1, ".", items[item])  # Prints all the items that is in the list
 
 
+# Define a method that takes the order
 def take_order(category):
-    available_items = menu_items(category)
-    display_items(available_items)
-    order_item = input(f"Please select a {category}: ")
+    available_items = menu_items(category)  # Get the available items from the menu items
+    display_items(available_items, category)  # Displays all the available items
+    order_item = input(f"Please select Your {category.capitalize()}: ")  # Customer selects what they want
     if order_item.isdigit():
-        return available_items[int(order_item)]
-         # order_list.append(available_items[int(order_item)])
+        return available_items[int(order_item)-1]  # the selected item is returned to be appended to order list
 
 
 order_list = []  # List to hold the order
 print("Welcome to \"XYZ\" Restaurant!")
-order_list.append(take_order("starter"))
-display_items(order_list)
-
+order_list.append(take_order("starter"))     # Starter Order
+order_list.append(take_order("mains"))       # Mains Order
+order_list.append(take_order("desserts"))    # Desserts Order
+display_items(order_list, "customer order")  # Displays the Customer Order
