124a9abaf816d1197b0c17c7b2e874d81438db25
Abhishek Jha <110366380+abhishek-jha-ce@users.noreply.github.com>
2022-10-14 15:18:38 +0100

    Update largest_number.py

--- practice-exercise/largest_number.py
+++ practice-exercise/largest_number.py
@@ -1,4 +1,14 @@
-# function to find the largest number
+# To find the largest number in the list
+# Sort the List
+# Return the Last number in the list - It will always be the largest in the sorted list
+
+def find_largest(numbers):
+    numbers.sort()
+    return numbers[-1]
+
+
+num = [3, 7, 8, 101, 12, 15, 3, 2]
+print(f"The largest number in the list: {find_largest(num)}")
 def find_largest(numbers):
     numbers.sort()  # Sorting the list
     return numbers[-1]  # The largest number will always be the last one
